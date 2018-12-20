bf110bde2f5f6e023abf8334a2213e6624ec90a9
Igglybuff <ed@edflix.club>
2018-12-20 17:49:37 +0000

    more entrypoint tweaks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
 INTERVAL="${INTERVAL:-7200}"
+
+if [ -z "${MREG_FILTER_NAME}" ]; then
+  echo "ERROR - you have not specified a filter name!"
+  exit 1
+fi
 
 while true; do
   echo "Running mreg..."
